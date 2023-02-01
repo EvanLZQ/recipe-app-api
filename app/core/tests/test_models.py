fed72cbd2a4b66737ee22e23d86c61e4adf41332
Evanabyss <evanabyss@gmail.com>
2023-02-01 18:24:16 -0500

    Implement recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -66,7 +66,7 @@ class ModelTests(TestCase):
             title='Sample recipe name',
             time_minutes=5,
             price=Decimal('5.50'),
-            decription='Sample recipe description',
+            description='Sample recipe description',
         )
 
         self.assertEqual(str(recipe), recipe.title)
